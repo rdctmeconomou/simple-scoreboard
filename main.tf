369e8467ec96ef77b7209650e9a59b50915b86ef
Matthew X. Economou <meconomou@researchdata.us>
2024-12-04 18:55:17 -0500

    feat: store players' scores in a DynamoDB table

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/lambda/${var.STACK_NAME}"
   retention_in_days = 3
 }
+
+resource "aws_dynamodb_table" "this" {
+  name         = "simple-scoreboard"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
