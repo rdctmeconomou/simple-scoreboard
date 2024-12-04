ea16e6d2aaacb0e5ccfc120321ba75111c0e95f3
Matthew X. Economou <meconomou@researchdata.us>
2024-12-04 18:14:17 -0500

    feat: lay out a standard OpenTofu module

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.80"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = jsondecode(var.TAGS_ALL)
+  }
+}
