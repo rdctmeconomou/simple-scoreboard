5d8c2a76a080076dd5f1059a01f9af252c76bb4a
Matthew X. Economou <meconomou@researchdata.us>
2024-12-04 19:35:06 -0500

    feat: grant the Lambda function read-only access to players' scores

--- main.tf
+++ main.tf
@@ -15,11 +15,15 @@ provider "aws" {
   }
 }
 
+data "aws_partition" "current" {}
+
+# Log app (Lambda function) activity in a CloudWatch log group.
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/lambda/${var.STACK_NAME}"
   retention_in_days = 3
 }
 
+# Store players' scores in a DynamoDB table.
 resource "aws_dynamodb_table" "this" {
   name         = "simple-scoreboard"
   billing_mode = "PAY_PER_REQUEST"
@@ -29,4 +33,54 @@ resource "aws_dynamodb_table" "this" {
     name = "name"
     type = "S"
   }
+}
+
+# Grant the Lambda function read-only access to players' scores.
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.${data.aws_partition.current.dns_suffix}"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = var.STACK_NAME
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["${aws_cloudwatch_log_group.this.arn}:log-stream:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:ConditionCheckItem",
+    ]
+    resources = [aws_dynamodb_table.this.arn]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name   = var.STACK_NAME
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
 }
