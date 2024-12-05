fb10728834bf30aba32685e57d7d7535052606e5
Matthew X. Economou <meconomou@researchdata.us>
2024-12-05 18:23:38 -0500

    refactor: adopt the Python src packaging flow

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_lambda_function" "this" {
   role             = aws_iam_role.this.arn
   filename         = "lambda-function.zip"
   source_code_hash = filebase64sha256("lambda-function.zip")
-  handler          = "lambda_function.lambda_handler"
+  handler          = "simple_scoreboard.lambda_handler"
   runtime          = "python3.12"
   architectures    = ["x86_64"]
 
